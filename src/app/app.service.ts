a1705c4f0e3f437ec8e9dea1db59d316242b7748
Maxim Korotenko <m.v.korotenko@gmail.com>
2018-10-16 11:08:18 +0300

    Code refactoring

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -3,78 +3,14 @@ import { HttpClient } from '@angular/common/http';
 
 import { map, tap } from 'rxjs/operators';
 
-const buildTree = function (node, parent?) {
-  if (parent) {
-    node.parent = parent;
-  }
-  if (node.childNodes) {
-    node.childNodes.forEach(n => buildTree(n, node));
-  }
-};
-
-const findAttrAll = function (node, val) {
-  if (node.attrs && node.attrs.some(a => a.value === val)) {
-    return node;
-  } else
-    if (node.childNodes) {
-      let res;
-      node.childNodes.some(n => {
-        res = findAttrAll(n, val);
-        return !!res;
-      });
-      return res;
-    }
-};
-
-const findAttrName = function (node, val, res?) {
-  const _res = res || [];
-  if (node.attrs && node.attrs.some(a => a.name === val)) {
-    _res.push(node);
-  }
-
-  if (node.childNodes) {
-    node.childNodes.forEach(n => findAttrName(n, val, _res));
-  }
-
-  return _res;
-};
-
-const getAttr = function (node, val) {
-  let res;
-  if (node.attrs && node.attrs.some(a => {
-    if (a.name === val) {
-      res = a.value;
-      return true;
-    }
-  })) {
-    return res;
-  } else
-    if (node.childNodes) {
-      node.childNodes.some(n => {
-        res = getAttr(n, val);
-        return !!res;
-      });
-      return res;
-    }
-};
-
-const getRatings = function (node) {
-  node.childNodes.forEach(n => {
-    const ratingNode = findAttrAll(n, 'current-rating');
-    if (ratingNode) {
-      n.rating = ratingNode.childNodes[0].value;
-    }
-  });
-};
-
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
 
-  public rootContent = this.client.get('api/acc/movies?rating_gt=3.9&details.Genre_or=Фэнтези,Боевик&till=20').pipe(
+  public rootContent = this.client.get('api/acc/movies?rating_gt=4.8&details.Genre_or=Фэнтези,Боевик&till=20').pipe(
     map((res: any[]) => {
-      const result = res.filter(e => e.rating >= 4);
+      const result = res;
       result.sort((a, b) => b.rating - a.rating);
       return result;
     }),
@@ -92,11 +28,6 @@ export class AppService {
 
   constructor(
     private client: HttpClient
-  ) {
-    window['getAttr'] = getAttr;
-    window['findAttr'] = findAttrAll;
-    window['findAttrName'] = findAttrName;
-    window['getRatings'] = getRatings;
-  }
+  ) { }
 
 }
