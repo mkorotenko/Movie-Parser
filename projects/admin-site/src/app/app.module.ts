1b0683036bb82ca7d2ebbc40a7a35f556e3915c8
unknown <m.v.korotenko@gmail.com>
2018-12-04 15:21:23 +0200

    Code restructure

--- projects/admin-site/src/app/app.module.ts
+++ projects/admin-site/src/app/app.module.ts
@@ -2,15 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { AdminService } from './admin/admin.service';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MonacoEditorModule, NgxMonacoEditorConfig } from 'ngx-monaco-editor';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/admin', pathMatch: 'full' },
+  { path: 'admin', component: AdminComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    MonacoEditorModule.forRoot()
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    AdminService
+  ],
+  exports: [ RouterModule ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- projects/common-site/app/app.module.ts
+++ projects/common-site/app/app.module.ts
@@ -9,24 +9,11 @@ import { AppComponent } from './app.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
 import { AppService } from './app.service';
 import { MovieCardComponent } from './movie-card/movie-card.component';
-import { AdminComponent } from './admin/admin.component';
-import { AdminService } from './admin/admin.service';
-
-import { MonacoEditorModule, NgxMonacoEditorConfig } from 'ngx-monaco-editor';
-
-// const monacoConfig: NgxMonacoEditorConfig = {
-//   baseUrl: 'app-name/assets', // configure base path for monaco editor
-//   defaultOptions: { scrollBeyondLastLine: false }, // pass default options to be used
-//   onMonacoLoad: () => {
-//     console.log((<any>window).monaco);
-//   } // here monaco object will be available as window.monaco use this function to extend monaco editor functionality.
-// };
 
 const routes: Routes = [
   { path: '', redirectTo: '/movies/1', pathMatch: 'full' },
   { path: 'movies', redirectTo: '/movies/1', pathMatch: 'full' },
   { path: 'movies/:id', component: MovieListComponent },
-  { path: 'admin', component: AdminComponent },
 ];
 
 @NgModule({
@@ -34,20 +21,17 @@ const routes: Routes = [
     AppComponent,
     MovieListComponent,
     MovieCardComponent,
-    AdminComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     RouterModule.forRoot(routes),
     HttpClientModule,
-    MonacoEditorModule.forRoot()
   ],
   providers: [
     AppService,
-    AdminService
   ],
   exports: [ RouterModule ],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
